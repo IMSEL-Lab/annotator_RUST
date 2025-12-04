251033cb2899b245184b7745504b80d94bafb3e6
j-vaught <jvaught@sc.edu>
2025-12-03 23:05:31 -0500

    Updated GUI to be Materail 3 style.

--- build.rs
+++ build.rs
@@ -1,3 +1,9 @@
 fn main() {
-    slint_build::compile("ui/appwindow.slint").unwrap();
+    let config = slint_build::CompilerConfiguration::new().with_library_paths(
+        std::collections::HashMap::from([(
+            "material".to_string(),
+            std::path::Path::new("ui/material/material.slint").to_path_buf(),
+        )]),
+    );
+    slint_build::compile_with_config("ui/appwindow.slint", config).unwrap();
 }
